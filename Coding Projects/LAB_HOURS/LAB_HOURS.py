5b2abf26e20fae9d296efa6b9c101c14835cc13e
tkiyohar <trentkiyohara@gmail.com>
2022-02-21 18:43:12 -0800

    added README files along with images and videos of previous cad and robotics projects

---
+++ Coding Projects/LAB_HOURS/LAB_HOURS.py
@@ -0,0 +1,190 @@
+#package installer
+import pip
+import subprocess
+
+packagecheck = subprocess.run('py -m pip list', capture_output=True)
+packagecheck = packagecheck.stdout.decode("utf-8")
+if "gspread" not in packagecheck:
+  subprocess.run('py -m pip install gspread')
+if "oauth2client" not in packagecheck:
+  subprocess.run('py -m pip install oauth2client')
+# -----------------------------------------------------------------------------------------------------------------------------
+# google sheet stuff:
+# client email: labot-367@humans-lab-hours.iam.gserviceaccount.com
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name('humans-lab-hours-00b010fa4155.json', scope)
+
+gc = gspread.authorize(credentials)
+
+# LS = actual Lab Sheet
+
+LS = gc.open('Lab_Hours').sheet1
+# -----------------------------------------------------------------------------------------------------------------------------
+# other packages and stuff
+import datetime
+from datetime import date
+import time
+import sys
+
+emptystring = ''
+# -----------------------------------------------------------------------------------------------------------------------------
+# Initialization
+TodayDateRow = 0
+PersonNameColumn = 0
+
+# DayZDonE function (Turning off lab hours program)
+def DayZDonE():
+    NotSignedOutColumn = list(LS.row_values(3))
+    NotSignedOutPeople = list()
+    if NotSignedOutColumn != ['Date:']:
+        NotSignedOutColumn.remove('Date:')
+        y=2
+        for x in NotSignedOutColumn:
+            if str(x):
+                NotSignedOutPeople.append(LS.cell(1, y).value)
+                LS.update_cell(3, y, '')
+            y += 1
+    else:
+        NotSignedOutPeople.append('Nobody :)')
+    print('Day concluded. The following people forgot to sign out and have thus forfeited their hours:')
+    for x in NotSignedOutPeople:
+        print(x)
+    sys.exit()
+
+# Date Initialization
+CurrentDate = str(date.today())
+Date_Column = LS.col_values(1)
+
+def date_initialization():
+    global TodayDateRow
+    if emptystring in Date_Column:
+        sys.exit('Error: Empty row in date column')
+    else:
+        blankdate = 4
+        datecell = LS.cell(blankdate, 1).value
+        while datecell:
+            if CurrentDate == datecell:
+                break
+            else:
+                blankdate += 1
+                datecell = LS.cell(blankdate, 1).value
+        TodayDateRow = blankdate
+        LS.update_cell(TodayDateRow, 1, CurrentDate)
+
+
+date_initialization()
+
+# Name initialization
+Name_Row = LS.row_values(1)
+if emptystring in Name_Row:
+    sys.exit('Error: Empty column in name row')
+else:
+    blankname = 2
+    namecell = LS.cell(1, blankname).value
+    while namecell:
+        blankname += 1
+        namecell = LS.cell(1, blankname).value
+# -----------------------------------------------------------------------------------------------------------------------------
+# name sign in part
+i = 0
+while i == 0:
+    nameinputsection = True
+    while nameinputsection == True:
+        while True:
+            name = input("\nPlease enter your real name: ")
+            if name == 'Name:':
+                print('Invalid, please enter your real name.')
+            elif name == 'Nobody :)':
+                print('Invalid, please enter your real name.')
+            elif name == 'DayZDonE':
+                DayZDonE()
+            else:
+                break
+        if name in Name_Row:
+            # person has signed in before
+            PersonNameColumn = int(LS.find(name).col)
+            nameinputsection = False
+        else:
+            # person hasn't signed on before
+            print('Hello ', name, ', if this is your first time signing in enter \"y\" or enter \"n\" to re-enter your name as it may be incorrect, (please note: consistent capitalization is important).')
+            firstsignin = input()
+            if firstsignin == 'y':
+                LS.update_cell(1, blankname, name)
+                print('\"', name, '\" has been added to the lab hours sheet.')
+                Name_Row.append(name)
+                print(Name_Row)
+                PersonNameColumn = blankname
+                blankname += 1
+                nameinputsection = False
+                break
+            elif firstsignin == 'n':
+                pass
+            # incorrect input (returns to name entry)
+            else:
+                print(
+                    'Invalid input, please enter either \"y\" if this is your first time signing in or \"n\" to re-enter your name')
+                pass
+    # -----------------------------------------------------------------------------------------------------------------------------
+    Signintime = str(LS.cell(3, PersonNameColumn).value)
+    CurrentTime = str(time.strftime('%X'))
+    if Signintime:
+        # signing out (Signintime not blank)
+        SignInHours = Signintime[:2]
+        SignInHours = int(SignInHours.lstrip('0'))
+        SignInMinutes = Signintime[3:-3]
+        SignInMinutes = SignInMinutes.lstrip('0')
+        if not SignInMinutes:
+            SignInMinutes = 0
+        SignInMinutes = int(SignInMinutes)
+        SignInSeconds = Signintime[-2:]
+        SignInSeconds = SignInSeconds.lstrip('0')
+        if not SignInSeconds:
+            SignInSeconds = 0
+        SignInSeconds = int(SignInSeconds)
+        SignOutHours = CurrentTime[:2]
+        SignOutHours = int(SignOutHours.lstrip('0'))
+        SignOutMinutes = CurrentTime[3:-3]
+        SignOutMinutes = SignOutMinutes.lstrip('0')
+        if not SignOutMinutes:
+            SignOutMinutes = 0
+        SignOutMinutes = int(SignOutMinutes)
+        SignOutSeconds = CurrentTime[-2:]
+        SignOutSeconds = SignOutSeconds.lstrip('0')
+        if not SignOutSeconds:
+            SignOutSeconds = 0
+        SignOutSeconds = int(SignOutSeconds)
+        EarlierSignInTime = str(LS.cell(TodayDateRow, PersonNameColumn).value)
+        if EarlierSignInTime:
+            EarlierSignInTime = int(EarlierSignInTime)
+        else:
+            EarlierSignInTime = 0
+
+        if SignOutHours < SignInHours:
+            SignOutHours += 24
+        # actual "signing out" functions with said variables
+        SignInTimeInSeconds = int(3600*(SignInHours) + 60*(SignInMinutes) + (SignInSeconds))
+        SignOutTimeInSeconds = int(3600*(SignOutHours) + 60*(SignOutMinutes) + (SignOutSeconds))
+        if 18000 in range(SignInTimeInSeconds, SignOutTimeInSeconds):
+            # Basically checking if the person's is trying to get lab hours for "working" at 5 am and if so, to discount them. (prevents getting crazy times for signing out then signing back in on a different day)
+            LS.update_cell(3, PersonNameColumn, '')
+            print('Sorry but it appears your forgot to sign out yesterday and thus your lab hours haven\'t been recorded. If you believe this to be an error please inform either Oscar or Trent otherwise please re-enter your name to sign in.')
+        elif 104400 in range(SignInTimeInSeconds, SignOutTimeInSeconds):
+            # Same as above, (checking for 5 am) but for 29 am, (5 am + 24 hours, byproduct of line 145)
+            LS.update_cell(3, PersonNameColumn, '')
+            print('Sorry but it appears your forgot to sign out yesterday and thus your lab hours haven\'t been recorded. If you believe this to be an error please inform either Oscar or Trent otherwise please re-enter your name to sign in.')
+        else:
+            TotalSignInTime = SignOutTimeInSeconds - SignInTimeInSeconds + EarlierSignInTime
+            LS.update_cell(TodayDateRow, PersonNameColumn, TotalSignInTime)
+            LS.update_cell(3, PersonNameColumn, '')
+            print('Thanks for comming ', name, ', you have been signed out at ', CurrentTime, ' on ', date.today())
+    else:
+        # signing in (Signintime blank)
+        LS.update_cell(3, PersonNameColumn, CurrentTime)
+        print('Hello ', name, ', you have been signed in at ', CurrentTime, ' on ', date.today())
+    # check that it is still the same day
+    if CurrentDate != str(date.today()):
+        date_initialization()
---
+++ Coding Projects/SorBa 2.0/ReadMe.txt
@@ -0,0 +1,37 @@
+SorBa 2.0             -made by Trent Horst Kiyohara
+--------------------------------------------------------------------------------------------------------------------------------------
+Setup:
+
+1. Download Python 3.7.3, (https://www.python.org/downloads/) and move the SorBa 2.0 folder to your desktop.
+
+2. Open whatever you are reading, (in chrome) followed by a spanishdict tab on the left hand side of the screen, (dont open the 
+spanishdict home but rather search a word first and make sure the work is highlighted in the translation bar. On the right side of the
+screen open a new study set in Quizlet, (see included screen shot for reference).
+
+3. Add a few blank words to quizlet till the "add new word" button is at the bottem of the screen and scroll down to the bottem of the
+page, (hotkeys require this for consistent positioning).
+
+--------------------------------------------------------------------------------------------------------------------------------------
+
+Hotkeys:
+
+Caps Lock:        Delete last word in quizlet and replace with a new slot
+Shift:            Enter selected word in quizlet
+Tab:              literally just tab plus scrolling down, (possitioning reasons).
+Number keys 1-3:  Select different words that appear on the left column in quizlet.
+Number key 4:     copy lowest left column in quizlet, (the word you just tried to translate) and pastes enters in to spanishdict,
+                  press 4 again to return to the pdf.
+Number Key 0:     Closes program
+
+--------------------------------------------------------------------------------------------------------------------------------------
+
+!!!Remember to open with python!!!
+To do this you can  right click the "SorBa_2.0.py" file and select to open with Python.
+
+Alternitivly on windows you can press the windows key + "r" to open a cmd window and enter said lines in the following order
+
+cd Desktop
+cd "SorBa 2.0"
+py SorBa_2.0.py
+
+If the program isn't working or you have any questions or suggestions, feel free to contact me at "tkiyohara@newroads.org".
