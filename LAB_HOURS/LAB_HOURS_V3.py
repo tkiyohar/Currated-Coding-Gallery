bf052e4b0f59ef74af34b6211582baea79536b48
tkiyohar <trentkiyohara@gmail.com>
2022-03-11 16:37:39 -0800

    black formatted all python files

--- LAB_HOURS/LAB_HOURS_V3.py
+++ LAB_HOURS/LAB_HOURS_V3.py
@@ -1,34 +1,41 @@
-#package installer
+# package installer
 import pip
 import subprocess
 
-packagecheck = subprocess.run('py -m pip list', capture_output=True)
+packagecheck = subprocess.run("py -m pip list", capture_output=True)
 packagecheck = packagecheck.stdout.decode("utf-8")
 if "gspread" not in packagecheck:
-  subprocess.run('py -m pip install gspread')
+    subprocess.run("py -m pip install gspread")
 if "oauth2client" not in packagecheck:
-  subprocess.run('py -m pip install oauth2client')
+    subprocess.run("py -m pip install oauth2client")
 # -----------------------------------------------------------------------------------------------------------------------------
 # google sheet stuff:
 # client email: labot-367@humans-lab-hours.iam.gserviceaccount.com
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+scope = [
+    "https://spreadsheets.google.com/feeds",
+    "https://www.googleapis.com/auth/drive",
+]
 
-credentials = ServiceAccountCredentials.from_json_keyfile_name('humans-lab-hours-00b010fa4155.json', scope)
+credentials = ServiceAccountCredentials.from_json_keyfile_name(
+    "humans-lab-hours-00b010fa4155.json", scope
+)
 
 gc = gspread.authorize(credentials)
 
 # LS = actual Lab Sheet
 
-LS = gc.open('Lab_Hours').sheet1
+LS = gc.open("Lab_Hours").sheet1
 
 # reconnect function:
 def connectionCheck():
     if credentials.access_token_expired:
-        print('reconnecting to google drive...')
+        print("reconnecting to google drive...")
         gc.login()
+
+
 # -----------------------------------------------------------------------------------------------------------------------------
 # other packages and stuff
 import datetime
@@ -36,36 +43,42 @@ from datetime import date
 import time
 import sys
 
-emptystring = ''
+emptystring = ""
 # -----------------------------------------------------------------------------------------------------------------------------
 # Special Functions
 
 # "help" command (Lists available commands)
 def help():
-    print("\nPlease note that all commands are lowercase and begin with a \"/\".")
+    print('\nPlease note that all commands are lowercase and begin with a "/".')
     print("The following is a list of commands you can execute in the program:\n")
-    print("\"/endday\": Ends the current day and discounts the hours of anyone who is still signed in (make sure everyone who is here has signed out before ending the day).")
-    print("\"/signedinlist\": Lists everyone who is currently signed in.")
-    print("\"/help\": Lists available commands.")
+    print(
+        '"/endday": Ends the current day and discounts the hours of anyone who is still signed in (make sure everyone who is here has signed out before ending the day).'
+    )
+    print('"/signedinlist": Lists everyone who is currently signed in.')
+    print('"/help": Lists available commands.')
+
 
 # "DayZDonE" command (Turning off lab hours program)
 def DayZDonE():
     NotSignedOutColumn = list(LS.row_values(3))
     NotSignedOutPeople = list()
-    if NotSignedOutColumn != ['Date:']:
-        NotSignedOutColumn.remove('Date:')
-        y=2
+    if NotSignedOutColumn != ["Date:"]:
+        NotSignedOutColumn.remove("Date:")
+        y = 2
         for x in NotSignedOutColumn:
             if str(x):
                 NotSignedOutPeople.append(LS.cell(1, y).value)
-                LS.update_cell(3, y, '')
+                LS.update_cell(3, y, "")
             y += 1
     else:
-        NotSignedOutPeople.append('Nobody :)')
-    print('Day concluded. The following people forgot to sign out and have thus forfeited their hours:')
+        NotSignedOutPeople.append("Nobody :)")
+    print(
+        "Day concluded. The following people forgot to sign out and have thus forfeited their hours:"
+    )
     for x in NotSignedOutPeople:
         print(x)
     sys.exit()
+
 
 # "who is signed in?" command (gives a list of everyone who is currently signed in)
 def whoIsSignedIn():
@@ -75,13 +88,15 @@ def whoIsSignedIn():
         if LS.cell(3, signInTimeColumn).value:
             studentsSignedInColumns.append(signInTimeColumn)
         signInTimeColumn += 1
-    print('the following people are currently signed in:')
-    #del studentsSignedInColumns[0]  # so "Name:" isn't counted as a member
+    print("the following people are currently signed in:")
+    # del studentsSignedInColumns[0]  # so "Name:" isn't counted as a member
     if studentsSignedInColumns != []:
         for x in studentsSignedInColumns:
             print(LS.cell(1, x).value)
     else:
-        print('Nobody :)')
+        print("Nobody :)")
+
+
 # -----------------------------------------------------------------------------------------------------------------------------
 # Initialization
 TodayDateRow = 0
@@ -95,13 +110,14 @@ Date_Column = LS.col_values(1)
 blankdate = 4
 CurrentDate = str(date.today())
 
+
 def date_initialization():
     global blankdate
     global TodayDateRow
     global CurrentDate
     CurrentDate = str(date.today())
     if emptystring in Date_Column:
-        sys.exit('Error: Empty row in date column')
+        sys.exit("Error: Empty row in date column")
     else:
         datecell = LS.cell(blankdate, 1).value
         while datecell:
@@ -113,12 +129,13 @@ def date_initialization():
         TodayDateRow = blankdate
         LS.update_cell(TodayDateRow, 1, CurrentDate)
 
+
 date_initialization()
 
 # Name initialization
 Name_Row = LS.row_values(1)
 if emptystring in Name_Row:
-    sys.exit('Error: Empty column in name row')
+    sys.exit("Error: Empty column in name row")
 else:
     blankname = 2
     namecell = LS.cell(1, blankname).value
@@ -132,23 +149,25 @@ while i == 0:
     nameinputsection = True
     while nameinputsection == True:
         while True:
-            name = input("\n\n\nPlease enter your real name or type \"/help\" for a list of commands: ")
+            name = input(
+                '\n\n\nPlease enter your real name or type "/help" for a list of commands: '
+            )
             connectionCheck()
             date_initialization()
-            if name == 'Name:':
-                print('Invalid, please enter your real name.')
-            elif name == 'Nobody :)':
-                print('Invalid, please enter your real name.')
+            if name == "Name:":
+                print("Invalid, please enter your real name.")
+            elif name == "Nobody :)":
+                print("Invalid, please enter your real name.")
             elif not name:
-                print('Invalid, please enter your real name.')
-            elif name == '/endday':
+                print("Invalid, please enter your real name.")
+            elif name == "/endday":
                 DayZDonE()
-            elif name == '/signedinlist':
+            elif name == "/signedinlist":
                 whoIsSignedIn()
-            elif name == '/help':
+            elif name == "/help":
                 help()
-            elif name[0] == '/':
-                print('Invalid, please enter your real name.')
+            elif name[0] == "/":
+                print("Invalid, please enter your real name.")
             else:
                 break
         if name in Name_Row:
@@ -157,52 +176,57 @@ while i == 0:
             nameinputsection = False
         else:
             # person hasn't signed on before
-            print('Hello ', name, ', if this is your first time signing in enter \"y\" or enter \"n\" to re-enter your name as it may be incorrect, (please note: consistent capitalization is important).')
+            print(
+                "Hello ",
+                name,
+                ', if this is your first time signing in enter "y" or enter "n" to re-enter your name as it may be incorrect, (please note: consistent capitalization is important).',
+            )
             firstsignin = input()
             connectionCheck()
-            if firstsignin == 'y':
+            if firstsignin == "y":
                 LS.update_cell(1, blankname, name)
                 NumberOfMembers.append(name)
-                print('\"', name, '\" has been added to the lab hours sheet.')
+                print('"', name, '" has been added to the lab hours sheet.')
                 Name_Row.append(name)
                 print(Name_Row)
                 PersonNameColumn = blankname
                 blankname += 1
                 nameinputsection = False
                 break
-            elif firstsignin == 'n':
+            elif firstsignin == "n":
                 pass
             # incorrect input (returns to name entry)
             else:
                 print(
-                    'Invalid input, please enter either \"y\" if this is your first time signing in or \"n\" to re-enter your name')
+                    'Invalid input, please enter either "y" if this is your first time signing in or "n" to re-enter your name'
+                )
                 pass
     # -----------------------------------------------------------------------------------------------------------------------------
     Signintime = str(LS.cell(3, PersonNameColumn).value)
-    CurrentTime = str(time.strftime('%X'))
+    CurrentTime = str(time.strftime("%X"))
     if Signintime:
         # signing out (Signintime not blank)
         SignInHours = Signintime[:2]
-        SignInHours = int(SignInHours.lstrip('0'))
+        SignInHours = int(SignInHours.lstrip("0"))
         SignInMinutes = Signintime[3:-3]
-        SignInMinutes = SignInMinutes.lstrip('0')
+        SignInMinutes = SignInMinutes.lstrip("0")
         if not SignInMinutes:
             SignInMinutes = 0
         SignInMinutes = int(SignInMinutes)
         SignInSeconds = Signintime[-2:]
-        SignInSeconds = SignInSeconds.lstrip('0')
+        SignInSeconds = SignInSeconds.lstrip("0")
         if not SignInSeconds:
             SignInSeconds = 0
         SignInSeconds = int(SignInSeconds)
         SignOutHours = CurrentTime[:2]
-        SignOutHours = int(SignOutHours.lstrip('0'))
+        SignOutHours = int(SignOutHours.lstrip("0"))
         SignOutMinutes = CurrentTime[3:-3]
-        SignOutMinutes = SignOutMinutes.lstrip('0')
+        SignOutMinutes = SignOutMinutes.lstrip("0")
         if not SignOutMinutes:
             SignOutMinutes = 0
         SignOutMinutes = int(SignOutMinutes)
         SignOutSeconds = CurrentTime[-2:]
-        SignOutSeconds = SignOutSeconds.lstrip('0')
+        SignOutSeconds = SignOutSeconds.lstrip("0")
         if not SignOutSeconds:
             SignOutSeconds = 0
         SignOutSeconds = int(SignOutSeconds)
@@ -215,25 +239,49 @@ while i == 0:
         if SignOutHours < SignInHours:
             SignOutHours += 24
         # actual "signing out" functions with said variables
-        SignInTimeInSeconds = int(3600*(SignInHours) + 60*(SignInMinutes) + (SignInSeconds))
-        SignOutTimeInSeconds = int(3600*(SignOutHours) + 60*(SignOutMinutes) + (SignOutSeconds))
+        SignInTimeInSeconds = int(
+            3600 * (SignInHours) + 60 * (SignInMinutes) + (SignInSeconds)
+        )
+        SignOutTimeInSeconds = int(
+            3600 * (SignOutHours) + 60 * (SignOutMinutes) + (SignOutSeconds)
+        )
         if 18000 in range(SignInTimeInSeconds, SignOutTimeInSeconds):
             # Basically checking if the person's is trying to get lab hours for "working" at 5 am and if so, to discount them. (prevents getting crazy times for signing out then signing back in on a different day)
-            LS.update_cell(3, PersonNameColumn, '')
-            print('Sorry but it appears your forgot to sign out yesterday and thus your lab hours haven\'t been recorded. If you believe this to be an error please inform either Oscar or Trent otherwise please re-enter your name to sign in.')
+            LS.update_cell(3, PersonNameColumn, "")
+            print(
+                "Sorry but it appears your forgot to sign out yesterday and thus your lab hours haven't been recorded. If you believe this to be an error please inform either Oscar or Trent otherwise please re-enter your name to sign in."
+            )
         elif 104400 in range(SignInTimeInSeconds, SignOutTimeInSeconds):
             # Same as above, (checking for 5 am) but for 29 am, (5 am + 24 hours, byproduct of line 145)
-            LS.update_cell(3, PersonNameColumn, '')
-            print('Sorry but it appears your forgot to sign out yesterday and thus your lab hours haven\'t been recorded. If you believe this to be an error please inform either Oscar or Trent otherwise please re-enter your name to sign in.')
+            LS.update_cell(3, PersonNameColumn, "")
+            print(
+                "Sorry but it appears your forgot to sign out yesterday and thus your lab hours haven't been recorded. If you believe this to be an error please inform either Oscar or Trent otherwise please re-enter your name to sign in."
+            )
         else:
-            TotalSignInTime = SignOutTimeInSeconds - SignInTimeInSeconds + EarlierSignInTime
+            TotalSignInTime = (
+                SignOutTimeInSeconds - SignInTimeInSeconds + EarlierSignInTime
+            )
             LS.update_cell(TodayDateRow, PersonNameColumn, TotalSignInTime)
-            LS.update_cell(3, PersonNameColumn, '')
-            print('Thanks for comming ', name, ', you have been signed out at ', CurrentTime, ' on ', date.today())
+            LS.update_cell(3, PersonNameColumn, "")
+            print(
+                "Thanks for comming ",
+                name,
+                ", you have been signed out at ",
+                CurrentTime,
+                " on ",
+                date.today(),
+            )
     else:
         # signing in (Signintime blank)
         LS.update_cell(3, PersonNameColumn, CurrentTime)
-        print('Hello ', name, ', you have been signed in at ', CurrentTime, ' on ', date.today())
+        print(
+            "Hello ",
+            name,
+            ", you have been signed in at ",
+            CurrentTime,
+            " on ",
+            date.today(),
+        )
     # check that it is still the same day
     if CurrentDate != str(date.today()):
         date_initialization()
--- SorBa 2.0/SorBa_2.0.py
+++ SorBa 2.0/SorBa_2.0.py
@@ -1,101 +1,102 @@
 import pip
 import subprocess
-packagecheck = subprocess.run('py -m pip list', capture_output=True)
+
+packagecheck = subprocess.run("py -m pip list", capture_output=True)
 packagecheck = packagecheck.stdout.decode("utf-8")
-if "PyAutoGUI" not in packagecheck: 
-  subprocess.run('py -m pip install PyAutoGUI')
-if "keyboard" not in packagecheck: 
-  subprocess.run('py -m pip install keyboard')
+if "PyAutoGUI" not in packagecheck:
+    subprocess.run("py -m pip install PyAutoGUI")
+if "keyboard" not in packagecheck:
+    subprocess.run("py -m pip install keyboard")
 
 import pyautogui
 import keyboard
 import time
 import webbrowser
 
-print("SorBa_2.0 is running... press the \"0\" key to quit.")
+print('SorBa_2.0 is running... press the "0" key to quit.')
 width, height = pyautogui.size()
-x, y = 640/2560*width, 1200/1600*height
+x, y = 640 / 2560 * width, 1200 / 1600 * height
 spanishdict = False
 
 while True:
-  if keyboard.is_pressed('shift'):
-    x, y = pyautogui.position()
-    time.sleep(0.3)
-    pyautogui.click(clicks=2)
-    pyautogui.hotkey('ctrl', 'c')
-    pyautogui.click(1583/2560*width, 1295/1600*height)
-    pyautogui.hotkey('ctrl', 'v')
-    pyautogui.press('tab')
-    time.sleep(0.3)
-    pyautogui.press('pagedown')
-  elif keyboard.is_pressed('1'):
-    pyautogui.press('backspace')
-    pyautogui.click(2195/2560*width, 1200/1600*height)
-    pyautogui.press('tab')
-    time.sleep(0.3)
-    pyautogui.press('pagedown')
-    pyautogui.moveTo(x, y)
-  elif keyboard.is_pressed('2'):
-    pyautogui.press('backspace')
-    pyautogui.click(2195/2560*width, 1245/1600*height)
-    pyautogui.press('tab')
-    time.sleep(0.3)
-    pyautogui.press('pagedown')
-    pyautogui.moveTo(x, y)
-  elif keyboard.is_pressed('3'):
-    pyautogui.press('backspace')
-    pyautogui.click(2195/2560*width, 1290/1600*height)
-    pyautogui.press('tab')
-    time.sleep(0.3)
-    pyautogui.press('pagedown')
-    pyautogui.moveTo(x, y)
-  elif keyboard.is_pressed('capslock'):
-    pyautogui.moveTo(1320/2560*width, 1290/1600*height)
-    pyautogui.click(1320/2560*width, 1290/1600*height)
-    time.sleep(0.15)
-    pyautogui.click(1583/2560*width, 1295/1600*height)
-    pyautogui.press('capslock')
-    pyautogui.press('tab')
-    pyautogui.press('tab')
-    time.sleep(0.3)
-    pyautogui.press('pagedown')
-    time.sleep(0.15)
-    pyautogui.click(1400/2560*width, 1192/1600*height)
-    pyautogui.click(1583/2560*width, 1295/1600*height)
-    time.sleep(0.3)
-    pyautogui.press('pagedown')
-    pyautogui.moveTo(x, y)
-  elif keyboard.is_pressed('tab'):
-    time.sleep(0.4)
-    pyautogui.press('pagedown')
-  elif keyboard.is_pressed('4'):
-    if spanishdict == False:
-      pyautogui.press('backspace')
-      pyautogui.moveTo(1320/2560*width, 1290/1600*height)
-      pyautogui.click(1320/2560*width, 1290/1600*height)
-      time.sleep(0.15)
-      pyautogui.click(1583/2560*width, 1295/1600*height, clicks=2)
-      pyautogui.hotkey('ctrl', 'c')
-      pyautogui.moveTo(1320/2560*width, 1290/1600*height)
-      pyautogui.click(1320/2560*width, 1290/1600*height)
-      time.sleep(0.15)
-      pyautogui.click(270/2560*width, 24/1600*height)
-      pyautogui.hotkey('ctrl', 'v')
-      pyautogui.press('enter')
-      time.sleep(0.3)
-      pyautogui.click(2330/2560*width, 1290/1600*height)
-      spanishdict = True
-      pyautogui.press('pagedown')
-    elif spanishdict == True:
-      pyautogui.press('backspace')
-      pyautogui.click(80/2560*width, 24/1600*height)
-      pyautogui.click(2330/2560*width, 1290/1600*height)
-      time.sleep(0.3)
-      pyautogui.press('pagedown')
-      pyautogui.moveTo(x, y)
-      spanishdict = False
-  elif keyboard.is_pressed('0'):
-    pyautogui.press('backspace')
-    break
-  else:
-    pass
+    if keyboard.is_pressed("shift"):
+        x, y = pyautogui.position()
+        time.sleep(0.3)
+        pyautogui.click(clicks=2)
+        pyautogui.hotkey("ctrl", "c")
+        pyautogui.click(1583 / 2560 * width, 1295 / 1600 * height)
+        pyautogui.hotkey("ctrl", "v")
+        pyautogui.press("tab")
+        time.sleep(0.3)
+        pyautogui.press("pagedown")
+    elif keyboard.is_pressed("1"):
+        pyautogui.press("backspace")
+        pyautogui.click(2195 / 2560 * width, 1200 / 1600 * height)
+        pyautogui.press("tab")
+        time.sleep(0.3)
+        pyautogui.press("pagedown")
+        pyautogui.moveTo(x, y)
+    elif keyboard.is_pressed("2"):
+        pyautogui.press("backspace")
+        pyautogui.click(2195 / 2560 * width, 1245 / 1600 * height)
+        pyautogui.press("tab")
+        time.sleep(0.3)
+        pyautogui.press("pagedown")
+        pyautogui.moveTo(x, y)
+    elif keyboard.is_pressed("3"):
+        pyautogui.press("backspace")
+        pyautogui.click(2195 / 2560 * width, 1290 / 1600 * height)
+        pyautogui.press("tab")
+        time.sleep(0.3)
+        pyautogui.press("pagedown")
+        pyautogui.moveTo(x, y)
+    elif keyboard.is_pressed("capslock"):
+        pyautogui.moveTo(1320 / 2560 * width, 1290 / 1600 * height)
+        pyautogui.click(1320 / 2560 * width, 1290 / 1600 * height)
+        time.sleep(0.15)
+        pyautogui.click(1583 / 2560 * width, 1295 / 1600 * height)
+        pyautogui.press("capslock")
+        pyautogui.press("tab")
+        pyautogui.press("tab")
+        time.sleep(0.3)
+        pyautogui.press("pagedown")
+        time.sleep(0.15)
+        pyautogui.click(1400 / 2560 * width, 1192 / 1600 * height)
+        pyautogui.click(1583 / 2560 * width, 1295 / 1600 * height)
+        time.sleep(0.3)
+        pyautogui.press("pagedown")
+        pyautogui.moveTo(x, y)
+    elif keyboard.is_pressed("tab"):
+        time.sleep(0.4)
+        pyautogui.press("pagedown")
+    elif keyboard.is_pressed("4"):
+        if spanishdict == False:
+            pyautogui.press("backspace")
+            pyautogui.moveTo(1320 / 2560 * width, 1290 / 1600 * height)
+            pyautogui.click(1320 / 2560 * width, 1290 / 1600 * height)
+            time.sleep(0.15)
+            pyautogui.click(1583 / 2560 * width, 1295 / 1600 * height, clicks=2)
+            pyautogui.hotkey("ctrl", "c")
+            pyautogui.moveTo(1320 / 2560 * width, 1290 / 1600 * height)
+            pyautogui.click(1320 / 2560 * width, 1290 / 1600 * height)
+            time.sleep(0.15)
+            pyautogui.click(270 / 2560 * width, 24 / 1600 * height)
+            pyautogui.hotkey("ctrl", "v")
+            pyautogui.press("enter")
+            time.sleep(0.3)
+            pyautogui.click(2330 / 2560 * width, 1290 / 1600 * height)
+            spanishdict = True
+            pyautogui.press("pagedown")
+        elif spanishdict == True:
+            pyautogui.press("backspace")
+            pyautogui.click(80 / 2560 * width, 24 / 1600 * height)
+            pyautogui.click(2330 / 2560 * width, 1290 / 1600 * height)
+            time.sleep(0.3)
+            pyautogui.press("pagedown")
+            pyautogui.moveTo(x, y)
+            spanishdict = False
+    elif keyboard.is_pressed("0"):
+        pyautogui.press("backspace")
+        break
+    else:
+        pass
